271c0970604a7e9c5cbb836b203ca117a72761a8
raaviakhila <141169583+raaviakhila@users.noreply.github.com>
2023-08-04 10:21:12 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/newsapplication/Model.kt
@@ -0,0 +1,6 @@
+package com.example.newsapplication
+
+data class Model(val name: String,
+                 val imageUrl: String,
+                 val desc: String,
+                 val category: String)
